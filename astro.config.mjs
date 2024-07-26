3312c2dbef45942c822a8612bfc6ebfe4a0c9579
jingwu2121 <jing.wu10.21@gmail.com>
2024-07-26 14:33:40 +0100

    add workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,7 +12,7 @@ import pagefind from "astro-pagefind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-micro-academic.vercel.app",
+  site: "https://jingwu2121.github.io",
   integrations: [tailwind(), sitemap(), mdx(), pagefind()],
   markdown: {
     shikiConfig: {
